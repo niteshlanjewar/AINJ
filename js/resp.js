ae6f4328bfb1225fee98e02072b73ffd7a22bcd4
niteshlanjewar <nitlanjewar1621@gmail.com>
2023-07-11 14:07:21 +0530

    ainj8

--- js/resp.js
+++ js/resp.js
@@ -1,11 +1,11 @@
-burger = document.querySelector('.burger')
-navbar = document.querySelector('.navbar')
-rightNav = document.querySelector('.rightNav')
-navList = document.querySelector('.nav-list')
+burger = document.querySelector('.burger');
+navbar = document.querySelector('.navbar');
+rightNav = document.querySelector('.rightNav');
+navList = document.querySelector('.nav-list');
 
 
 
-burger.addEventlistener("click", ()=>{
+burger.addEventListener('click', ()=>{
     rightNav.classList.toggle('visibility-resp');
     navList.classList.toggle('visibility-resp');
     navbar.classList.toggle('ht-nav-resp');
